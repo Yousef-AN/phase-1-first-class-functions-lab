be1d94cefb250d0c01473a399c607e4c775efc34
Yousef. <kingyousef922@gmail.com>
2023-10-05 18:23:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);};
+
+const returnLastTwoDrivers = function(drivers) {
+        return drivers.slice(2, 4);}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function  createFareMultiplier(n){
+   return function () {
+         return n*n //
+}}
+
+function fareDoubler(n){ 
+    return n * 2
+}
+
+function fareTripler(n){
+    return n * 3
+}
+
+function selectDifferentDrivers(drivers, func) {
+    return func(drivers)
+}
